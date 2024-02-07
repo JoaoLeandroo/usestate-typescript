b0b5bf9a9b05f08a7a54d0be80eaf706c53da696
joaoleandro <new.joaoleandro@gmail.com>
2024-02-07 15:40:44 -0300

    usestate + typescript

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [nome, setNome] = useState<string | null>("");
+
+  function mudar() {
+    const novoNome: string | null = prompt("Digite um nome");
+    if(novoNome === "") {
+      setNome("Informe um nome")
+      return
+    }
+    setNome(novoNome);
+  }
+
+  return (
+    <div className="container">
+      <h1>{nome}</h1>
+      <button onClick={mudar}>Alterar Nome</button>
+    </div>
+  );
+}
+
+export default App;
